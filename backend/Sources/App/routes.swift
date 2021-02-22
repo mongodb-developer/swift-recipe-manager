acf4a509166bf74f682298c47b1d3d8febaca48c
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-22 13:39:39 -0800

    Undo the "wrong" way to do codables

--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -1,18 +1,12 @@
 import MongoDBVapor
 import Vapor
 
-extension Recipe: Content {}
-
 /// Adds a collection of routes to the application.
 func routes(_ app: Application) throws {
     app.get { req -> String in
         "hello, world!"
     }
-    app.get("api", "recipes") { req -> Recipe in
-        return Recipe(
-            title: "Cookies",
-            servings: 6,
-            calories: 300
-        )
+    app.get("api", "recipes") { req -> String in
+        return "Hello"
     }
 }
