b333adcf70354682c44504335cc212d5539776e5
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-22 12:06:03 -0800

    Configure the React Router and give it a quick theme

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <main class="container mx-auto pt-24 px-4">
+            <p>Welcome to the home page!</p>
+        </main>
+    );
+}
+
+export default Home;
