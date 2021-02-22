6d85a4359f4067f6503fe23dec9e636b77d916fc
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-22 13:31:33 -0800

    start codable for recipe and add a test api endpoint

--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -1,9 +1,18 @@
 import MongoDBVapor
 import Vapor
 
+extension Recipe: Content {}
+
 /// Adds a collection of routes to the application.
 func routes(_ app: Application) throws {
     app.get { req -> String in
         "hello, world!"
+    }
+    app.get("api", "recipes") { req -> Recipe in
+        return Recipe(
+            title: "Cookies",
+            servings: 6,
+            calories: 300
+        )
     }
 }
