9fb50f8f83825bb6d83fdfd949df1f56e3efe52d
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-23 08:18:07 -0800

    Mess around with an endpoint that takes a url route parameter and does a basic aggregation pipeline
    ground work for fts

--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -1,14 +1,30 @@
 import MongoDBVapor
 import Vapor
 
-/// Adds a collection of routes to the application.
+extension BSONDocument: Content{}
+
 func routes(_ app: Application) throws {
+
     app.get { req -> String in
         "hello, world!"
     }
+
     app.get("api", "recipes") { req -> EventLoopFuture<[Recipe]> in
         return req.recipeCollection.find().flatMap { cursor in
             cursor.toArray()
         }
     }
+
+    app.get("api", "recipe", ":title") { req -> EventLoopFuture<[BSONDocument]> in
+        guard let title = req.parameters.get("title") else {
+            throw Abort(.internalServerError, reason: "Request unexpectedly missing name parameter")
+        }
+        let pipeline: [BSONDocument] = [
+            ["$match": [ "title": .string(title) ]]
+        ]
+        return req.recipeCollection.aggregate(pipeline).flatMap { cursor in
+            cursor.toArray()
+        }
+    }
+    
 }
