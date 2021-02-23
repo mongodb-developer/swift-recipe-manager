722e04745f7405209a4b4930723e2f410c01dc49
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-23 09:24:15 -0800

    Add CORS to Vapor. Consume Recipes in React.
    Minor updates to the TailwindCSS styles

--- backend/Sources/App/configure.swift
+++ backend/Sources/App/configure.swift
@@ -6,6 +6,15 @@ public func configure(_ app: Application) throws {
     // serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+
+    app.middleware.use(corsMiddleware)
+
     // register routes
     try routes(app)
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <main class="container mx-auto pt-24 px-4">
+        <main className="container mx-auto pt-24 px-4">
             <p>Welcome to the home page!</p>
         </main>
     );
