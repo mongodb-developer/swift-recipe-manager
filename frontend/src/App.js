aabf198cebf388ae83f0f1d54c73bbe100170b94
Nic Raboy <nicolas.raboy@mongodb.com>
2021-02-24 14:15:27 -0800

    Add Searching to React UX

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,72 @@
-import React from "react";
+import { useState } from "react";
 import {
-    BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    useHistory,
 } from "react-router-dom";
 import Recipes from "./pages/recipes";
 import Recipe from "./pages/recipe";
 import Home from "./pages/home";
+import Search from "./pages/search";
 
 function App() {
-    const [isNavbarOpen, setIsNavbarOpen] = React.useState(false);
+
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+    const history = useHistory();
+
+    function performSearch(event) {
+        if(event.key === "Enter") {
+            if(event.target.value !== "") {
+                history.push("/search/" + event.target.value);
+            }
+        }
+    }
+
     return (
-        <Router>
-            <div className="flex flex-col min-h-screen">
-                <header className="fixed bg-mongodb-forest border-b-4 border-mongodb-fern w-full z-50 text-white">
-                    <nav className="container mx-auto flex items-center justify-between flex-wrap p-4">
-                        <div className="flex items-center flex-shrink-0 text-white mr-6">
-                            <span className="font-bold text-xl uppercase"><a className="hover:no-underline" href="/" title="BuildFest 2021">BuildFest 2021</a></span>
-                        </div>
-                        <div className="block sm:hidden">
-                            <button onClick={() => setIsNavbarOpen(!isNavbarOpen)} aria-label="Navigation Menu" className="flex items-center py-2 rounded text-white">
-                                <svg viewBox="0 0 100 80" width="24" height="24" fill="#FFFFFF">
-                                    <rect width="100" height="15"></rect>
-                                    <rect y="30" width="100" height="15"></rect>
-                                    <rect y="60" width="100" height="15"></rect>
-                                </svg>
-                            </button>
-                        </div>
-                        <div id="nav-items" className={(isNavbarOpen ? "flex " : "hidden ") + "w-full block flex-grow sm:flex sm:items-center sm:w-auto text-left sm:text-right font-bold border-t border-white mt-5 sm:mt-0 sm:border-0 sm:block"}>
-                            <div className="text-sm sm:flex-grow">
-                                <a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-white uppercase sm:ml-6 hover:underline">Home</a>
-                                <a href="/recipes" className="block mt-4 sm:inline-block sm:mt-0 text-white uppercase sm:ml-6 hover:underline">Recipes</a>
-                            </div>
-                        </div>
-                    </nav>
-                </header>
-                <Switch>
-                    <Route path="/recipes">
-                        <Recipes />
-                    </Route>
-                    <Route path="/recipe/:id">
-                        <Recipe />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-                <footer className="bg-gray-700 text-white text-xs">
-                    <div className="container mx-auto p-4">
-                        Created by <a target="_blank" rel="noreferrer" href="https:/www.nraboy.com">Nic Raboy</a>, <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mlynn/">Mike Lynn</a>, and <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kaitlinmahar/">Kaitlin Mahar</a>
+        <div className="flex flex-col min-h-screen">
+            <header className="fixed bg-mongodb-forest border-b-4 border-mongodb-fern w-full z-50 text-white">
+                <nav className="container mx-auto flex items-center justify-between flex-wrap p-4">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        <span className="font-bold text-xl uppercase"><a className="hover:no-underline" href="/" title="BuildFest 2021">BuildFest 2021</a></span>
                     </div>
-                </footer>
-            </div>
-        </Router>
+                    <div className="block sm:hidden">
+                        <button onClick={() => setIsNavbarOpen(!isNavbarOpen)} aria-label="Navigation Menu" className="flex items-center py-2 rounded text-white">
+                            <svg viewBox="0 0 100 80" width="24" height="24" fill="#FFFFFF">
+                                <rect width="100" height="15"></rect>
+                                <rect y="30" width="100" height="15"></rect>
+                                <rect y="60" width="100" height="15"></rect>
+                            </svg>
+                        </button>
+                    </div>
+                    <div id="nav-items" className={(isNavbarOpen ? "sm:flex " : "hidden ") + "w-full block flex-grow sm:flex sm:items-center sm:w-auto text-left sm:text-right font-bold border-t border-white mt-5 sm:mt-0 sm:border-0 sm:block"}>
+                        <div className="text-sm sm:flex-grow">
+                            <input onKeyPress={performSearch} className="sm:ml-6 mt-4 sm:mt-0 p-2 w-full sm:w-1/2 md:w-1/3 text-black" type="text" placeholder="Search..." />
+                            <a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-white uppercase sm:ml-6 hover:underline">Home</a>
+                            <a href="/recipes" className="block mt-4 sm:inline-block sm:mt-0 text-white uppercase sm:ml-6 hover:underline">Recipes</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <Switch>
+                <Route path="/recipes">
+                    <Recipes />
+                </Route>
+                <Route path="/recipe/:id">
+                    <Recipe />
+                </Route>
+                <Route path="/search/:query">
+                    <Search />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+            <footer className="bg-gray-700 text-white text-xs">
+                <div className="container mx-auto p-4">
+                    Created by <a target="_blank" rel="noreferrer" href="https:/www.nraboy.com">Nic Raboy</a>, <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mlynn/">Mike Lynn</a>, and <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kaitlinmahar/">Kaitlin Mahar</a>
+                </div>
+            </footer>
+        </div>
     );
 }
 
